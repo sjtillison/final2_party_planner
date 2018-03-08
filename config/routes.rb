c49a04ab6afbd298b30ed2bbd1a167d8cbbbf22e
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:51:30 +0000

    Generate activities resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Activity resource:
+  # CREATE
+  get "/activities/new", :controller => "activities", :action => "new"
+  post "/create_activity", :controller => "activities", :action => "create"
+
+  # READ
+  get "/activities", :controller => "activities", :action => "index"
+  get "/activities/:id", :controller => "activities", :action => "show"
+
+  # UPDATE
+  get "/activities/:id/edit", :controller => "activities", :action => "edit"
+  post "/update_activity/:id", :controller => "activities", :action => "update"
+
+  # DELETE
+  get "/delete_activity/:id", :controller => "activities", :action => "destroy"
+  #------------------------------
+
   # Routes for the Meal_time resource:
   # CREATE
   get "/meal_times/new", :controller => "meal_times", :action => "new"
