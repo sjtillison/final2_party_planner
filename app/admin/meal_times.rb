d49a9755a8ac57303185c60181f254cf16b4983b
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:52:43 +0000

    Whitelist columns in MealTime for ActiveAdmin

--- app/admin/meal_times.rb
+++ app/admin/meal_times.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register MealTime do
+
+ permit_params :meal
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
